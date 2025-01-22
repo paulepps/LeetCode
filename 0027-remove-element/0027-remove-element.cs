09c6c4168b1f8ee20dbd9a65deb9ed3f3b1cc811
Paul Epps <paul@eppsnet.com>
2025-01-21 18:58:40 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int i = 0;
+
+        for (int j = 0; j < nums.length(); j++) {
+            if (nums[j] != val) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+        }
+        return i;
+    }
+}
