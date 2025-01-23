acd40912d5b30fb30e699bf6528410b876010b47
Paul Epps <paul@eppsnet.com>
2025-01-22 19:00:50 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -0,0 +1,17 @@
+function productExceptSelf(nums: number[]): number[] {
+    const: number[] answer = [];
+    let curr: number = 1;
+
+    for (let i: number = 0; i < nums.length; i++) {
+        answer[i] = curr;
+        curr *= nums[i];
+    }    
+
+    curr = 1;
+    for (let i: number = nums.length - 1; i >= 0; i--) {
+        answer[i] *= curr;
+        curr *= nums[i];
+    }
+
+    return answer;        
+};
