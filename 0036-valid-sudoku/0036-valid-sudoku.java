ae08d620c900b0745091e2b215fc9a6436af0508
Paul Epps <paul@eppsnet.com>
2025-01-27 21:55:12 -0800

    Time: 1 ms (100%), Space: 44.5 MB (67.7%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] columns = new boolean[9][9];
+        boolean[][] subgrids = new boolean[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char currentChar = board[i][j];
+
+                if (currentChar == '.')
+                    continue;
+
+                int number = (currentChar - '0' - 1);
+
+                int sgIndex = (i / 3) * 3 + j / 3;
+
+                if (rows[i][number] || columns[j][number] || subgrids[sgIndex][number]) {
+                    return false;
+                }
+
+                rows[i][number]  = true;
+                columns[j][number]  = true;
+                subgrids[sgIndex][number] = true;
+            }
+        }
+        return true;
+    }
+}
