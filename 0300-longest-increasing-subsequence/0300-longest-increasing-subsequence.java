6b6712a5684a7ab3a53aca317b85b32166a62005
Paul Epps <paul@eppsnet.com>
2025-01-30 16:46:06 -0800

    Time: 79 ms (22.67%), Space: 44.2 MB (64.84%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -4,23 +4,31 @@ class Solution {
         int n = nums.length;
         int result = 1;
 
+        int[] memo = new int[n];
+        Arrays.fill(memo, -1);
+
         for (int i = 1; i < n; i++) {
-            result = Math.max(result, lisEndingAtI(nums, i));
+            result = Math.max(result, lisEndingAtI(nums, i, memo));
         }
         return result;
     }
 
-    private int lisEndingAtI(int[] arr, int idx) {
+    private int lisEndingAtI(int[] arr, int idx, int[] memo) {
         if (idx == 0) 
             return 1;
+
+        if (memo[idx] != -1) {
+            return memo[idx];
+        }
 
         int max = 1;
 
         for (int prev = 0; prev < idx; prev++) {
             if (arr[prev] < arr[idx]) {
-                max = Math.max(max, lisEndingAtI(arr, prev) + 1);
+                max = Math.max(max, lisEndingAtI(arr, prev, memo) + 1);
             }
         }
+        memo[idx] = max;
         return max;
     }
 }
