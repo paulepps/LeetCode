025777dd2a77a32fe138f23324805542d1a9aab9
Paul Epps <paul@eppsnet.com>
2025-02-02 16:02:06 -0800

    Time: 2 ms (21.2%), Space: 40.7 MB (75.67%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> s = new HashSet<>();
+
+        while (true) {
+            n = sumSquareDigits(n);
+            if (n == 1)
+                return true;
+            if (s.contains(n))
+                return false;
+            s.add(n);
+        }
+    }
+
+    int sumSquareDigits(int n) {
+        int num = 0;
+        while (n != 0) {
+            int digit = n % 10;
+            num += digit * digit;
+            n /= 10;
+        }
+        return num;
+    }
+}
