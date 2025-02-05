d070f0d4ac429ed003d5ef0db27bfef6e542f4b0
Paul Epps <paul@eppsnet.com>
2025-02-04 18:14:19 -0800

    Time: 0 ms (100%), Space: 42.3 MB (7.41%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -4,23 +4,18 @@ class Solution {
         int numRows = obstacleGrid.length;
         int numCols = obstacleGrid[0].length;
 
-        int[][] dp = new int[numRows][numCols];
+        int[] dp = new int[numCols];
+        dp[0] = 1;
 
-        for (int row = 0; row < numRows && obstacleGrid[row][0] == 0; row++) {
-            dp[row][0] = 1;
-        }
-
-        for (int col = 0; col < numCols && obstacleGrid[0][col] == 0; col++) {
-            dp[0][col] = 1;
-        }
-
-        for (int row = 1; row < numRows; row++) {
-            for (int col = 1; col < numCols; col++) {
-                if (obstacleGrid[row][col] == 0) {
-                    dp[row][col] = dp[row - 1][col] + dp[row][col - 1];
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                if (obstacleGrid[row][col] == 1) {
+                    dp[col] = 0;
+                } else if (col > 0) {
+                    dp[col] += dp[col - 1];
                 }
             }
         }
-        return dp[numRows - 1][numCols - 1];
+        return dp[numCols - 1];
     }
 }
