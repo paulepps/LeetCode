8b296b32e2ebc3a409f9efa83a5e7bce59548ea4
Paul Epps <paul@eppsnet.com>
2025-02-10 15:36:52 -0800

    Time: 11 ms (34.23%), Space: 43.7 MB (74.22%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int calculate(String s) {
+        
+        int sum = 0; 
+        int sign = 1;
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                int val = 0;
+                while (i < s.length() && Character.isDigit(s.charAt(i))) {
+                    val = val * 10 + (s.charAt(i) - '0');
+                    i++;
+                }
+                sum += val * sign;
+                sign = 1;
+                i--;
+            } else if (c == '-') {
+                sign *= -1;
+            } else if (c == '(') {
+                stack.push(sum);
+                stack.push(sign);
+                sum = 0;
+                sign = 1;
+            } else if (c == ')') {
+                int poppedSign = stack.pop();
+                int prevSum = stack.pop();
+                sum = (sum * poppedSign) + prevSum;
+            }
+        }
+        return sum;
+    }
+}
