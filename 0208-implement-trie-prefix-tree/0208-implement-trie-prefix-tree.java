a05bf81c8c791edd7a4ccee7a2e1a885aefbf2ae
Paul Epps <paul@eppsnet.com>
2025-02-14 18:52:00 -0800

    Time: 34 ms (76.15%), Space: 55.8 MB (26.02%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -0,0 +1,64 @@
+class TrieNode {
+    TrieNode[] children;
+    boolean isEndOfWord;
+
+    public TrieNode() {
+        children = new TrieNode[26];
+        isEndOfWord = false;
+    }
+}
+class Trie {
+
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();    
+    }
+    
+    public void insert(String word) {
+        TrieNode current = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (current.children[c - 'a'] == null) {
+                current.children[c - 'a'] = new TrieNode();
+            }
+            current = current.children[c - 'a'];
+        }
+        current.isEndOfWord = true;
+    }
+    
+    public boolean search(String word) {
+        TrieNode current = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (current.children[c - 'a'] == null) {
+                return false;
+            }
+            current = current.children[c - 'a'];
+        }
+        return current.isEndOfWord;
+    }
+    
+    public boolean startsWith(String prefix) {
+        TrieNode current = root;
+
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (current.children[c - 'a'] == null) {
+                return false;
+            }
+            current = current.children[c - 'a'];
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
