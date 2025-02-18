9657717e4d6706052229709538b9d0e36f4689f2
Paul Epps <paul@eppsnet.com>
2025-02-17 20:21:12 -0800

    Time: 2 ms (57.39%), Space: 58.3 MB (49.99%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let localMax = nums[0];
+    let globalMax = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        localMax = Math.max(nums[i], localMax + nums[i]);
+        if (localMax > globalMax) {
+            globalMax = localMax;
+        }
+    }
+    return globalMax;
+    
+};
