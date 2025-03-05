95c59d28e7c5895300d0933739c97d4e7c9ec888
Paul Epps <paul@eppsnet.com>
2025-03-05 14:53:48 -0800

    Time: 0 ms (100%), Space: 55 MB (23.11%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.ts
@@ -0,0 +1,17 @@
+function isSubsequence(s: string, t: string): boolean {
+        let n = s.length;
+        let m = t.length;
+
+        if (n > m) return false;
+
+        let i = 0;
+        let j = 0;
+
+        while (i < n && j < m) {
+            if (s[i] == t[j]) {
+                i++;
+            }
+            j++;
+        }
+        return i == n;
+};
