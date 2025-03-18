8e0b6bbfc19149a6aba4d18a705ab3af6254441c
Paul Epps <paul@eppsnet.com>
2025-03-18 13:45:02 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) Arrays.sort(nums);
+      
+        int closestSum = Integer.MAX_VALUE;
+        int length = nums.length;
+      
+        for (int i = 0; i < length; ++i) {
+            int left = i + 1;
+            int right = length - 1;
+          
+            while (left < right) {
+                int currentSum = nums[i] + nums[left] + nums[right];
+              
+                if (currentSum == target) {
+                    return currentSum;
+                }
+              
+                if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+                    closestSum = currentSum;
+                }
+              
+                if (currentSum > target) {
+                    --right;
+                } else {
+                    ++left;
+                }
+            }
+        }
+      
+        // Return the closest sum found
+        return closestSum;
+    }
+ }
