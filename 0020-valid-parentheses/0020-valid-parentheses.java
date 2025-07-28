47a007139751f41ba22f7a56aaa10fec393bb302
Paul Epps <paul@eppsnet.com>
2025-07-28 16:01:38 -0700

    Time: 1 ms (99.45%), Space: 41.2 MB (99.78%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty() || !match(stack.pop(), c)) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    boolean match(char left, char right) {
+        return (left == '(' && right == ')') ||
+               (left == '{' && right == '}') ||
+               (left == '[' && right == ']');
+    }
+}
