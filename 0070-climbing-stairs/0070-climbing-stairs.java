930293cf3eedcb6800246d3f2b082007d700fc75
Paul Epps <paul@eppsnet.com>
2025-08-05 17:12:55 -0700

    Time: 0 ms (100%), Space: 40.3 MB (81.98%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        int prev = 1;
+        int curr = 1;
+
+        for (int i = 1; i < n; i++) {
+            int next = prev + curr;
+
+            prev = curr;
+            curr = next;
+        }
+        return curr;
+    }
+}
