8ce6e1ef654cb4ff6d8185c5ed84751edeb0cee4
Paul Epps <paul@eppsnet.com>
2025-08-08 13:34:57 -0700

    Time: 0 ms (100%), Space: 42 MB (83.62%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return binarySearch(nums, 0, nums.length - 1, target);
+    }
+
+    private int binarySearch(int[] nums, int lo, int hi, int target) {
+        if (lo > hi) {
+            return -1;
+        }
+
+        int mid = (lo + hi) / 2;
+
+        if (target == nums[mid]) {
+            return mid;
+        }
+    
+        if (nums[lo] <= nums[mid]) { // left half is sorted
+            if(nums[lo] <= target && target < nums[mid]) {
+                return binarySearch(nums, lo, mid-1, target);
+            } else {
+                return binarySearch(nums, mid+1, hi, target);
+            }
+        } else { // right half is sorted
+            if(nums[mid] < target && target <= nums[hi]) {
+                return binarySearch(nums, mid+1, hi, target);
+            } else {
+                return binarySearch(nums, lo, mid-1, target);        
+            }
+        }
+    }
+}
