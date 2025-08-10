dc4894e9a280032956e3b2c6132290a4218198ae
Paul Epps <paul@eppsnet.com>
2025-08-10 11:48:27 -0700

    Time: 5 ms (99.22%), Space: 46.2 MB (21.65%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,9 +1,5 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return false;
-        }
-
         int row = 0;
         int col = matrix[0].length - 1;
 
