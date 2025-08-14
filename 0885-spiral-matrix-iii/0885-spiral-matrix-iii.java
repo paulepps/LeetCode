5d8aa828eef61d9bf2483d3c6ceb37637f0f19c4
Paul Epps <paul@eppsnet.com>
2025-08-14 15:47:17 -0700

    Time: 3 ms (96.79%), Space: 45.5 MB (40.44%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.java
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.java
@@ -1,7 +1,16 @@
 class Solution {
+
+    private int rows;
+    private int cols;
+    private int result[][];
+    private int cellCount;
+
     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
         
-        int[][] result = new int[rows * cols][2];
+        this.rows = rows;
+        this.cols = cols;
+
+        result = new int[rows * cols][2];
         result[0][0] = rStart;
         result[0][1] = cStart;
 
@@ -9,48 +18,40 @@ class Solution {
         int col = cStart;
 
         int legLength = 1;
-        int cellCount = 1;
+        cellCount = 1;
         
         while (cellCount < rows * cols) {
             // left
             for (int i = 0; i < legLength; i++) {
                 col++;
-                if (row >= 0 && row < rows && col >= 0 && col < cols) {
-                    result[cellCount][0] = row;
-                    result[cellCount][1] = col;
-                    cellCount++;
-                }
+                checkAndAdd(row, col);
             }
             // down
             for (int i = 0; i < legLength; i++) {
                 row++;
-                if (row >= 0 && row < rows && col >= 0 && col < cols) {
-                    result[cellCount][0] = row;
-                    result[cellCount][1] = col;
-                    cellCount++;
-                }
+                checkAndAdd(row, col);
             }
             legLength++;
             // right
             for (int i = 0; i < legLength; i++) {
                 col--;
-                if (row >= 0 && row < rows && col >= 0 && col < cols) {
-                    result[cellCount][0] = row;
-                    result[cellCount][1] = col;
-                    cellCount++;
-                }
+                checkAndAdd(row, col);
             }
             // up
             for (int i = 0; i < legLength; i++) {
                 row--;
-                if (row >= 0 && row < rows && col >= 0 && col < cols) {
-                    result[cellCount][0] = row;
-                    result[cellCount][1] = col;
-                    cellCount++;
-                }
+                checkAndAdd(row, col);
             }
             legLength++;
         }
         return result;
+    }
+
+    private void checkAndAdd(int row, int col) {
+        if (row >= 0 && row < rows && col >= 0 && col < cols) {
+            result[cellCount][0] = row;
+            result[cellCount][1] = col;
+            cellCount++;
+        }
     }
 }
