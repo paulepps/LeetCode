65ea270318229734761f5832975ff26b8542e6f2
Paul Epps <paul@eppsnet.com>
2025-08-20 11:47:59 -0700

    Time: 4 ms (83.76%), Space: 60.2 MB (66.24%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -13,27 +13,22 @@ class Solution {
         ListNode slow = head;
         ListNode fast = head;
 
-        // Move fast pointer to the end, and slow to the middle of the list
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
 
-        // Compare the reversed second half with the first half
         ListNode firstHalf = head;
         ListNode secondHalf = reverseList(slow);
         while (secondHalf != null) {
-            // If values are different, then it's not a palindrome
             if (secondHalf.val != firstHalf.val) {
                 return false;
             }
 
-            // Move to the next nodes in both halves
             secondHalf = secondHalf.next;
             firstHalf = firstHalf.next;
         }
 
-        // All values matched, so it's a palindrome
         return true;
     }
 
@@ -41,10 +36,10 @@ class Solution {
         ListNode prev = null;
 
         while (current != null) {
-            ListNode temp = current.next; // Stores the next node
-            current.next = prev; // Reverses the link
-            prev = current; // Moves prev to current node
-            current = temp; // Move to the next node in the original list
+            ListNode temp = current.next;
+            current.next = prev;
+            prev = current; 
+            current = temp; 
         }
         return prev;
     }
