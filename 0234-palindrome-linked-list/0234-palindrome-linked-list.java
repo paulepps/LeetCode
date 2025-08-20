658ab810d17e758c8e784f681cf7b1c35e717b89
Paul Epps <paul@eppsnet.com>
2025-08-19 17:05:45 -0700

    Time: 3 ms (99.74%), Space: 68.7 MB (38.67%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,31 +10,40 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode fast = head;
         ListNode slow = head;
+        ListNode fast = head;
 
-        Deque<Integer> stack = new ArrayDeque<>();
-
+        // Move fast pointer to the end, and slow to the middle of the list
         while (fast != null && fast.next != null) {
-            stack.push(slow.val);
-            fast = fast.next.next;
             slow = slow.next;
+            fast = fast.next.next;
         }
 
-        ListNode node;
-
-        if (fast == null) {
-            node = slow;
-        } else {
-            node = slow.next;
+        // Reverse the second half of the list
+        ListNode prev = null;
+        ListNode current = slow;
+        while (current != null) {
+            ListNode temp = current.next; // Stores the next node
+            current.next = prev; // Reverses the link
+            prev = current; // Moves prev to current node
+            current = temp; // Move to the next node in the original list
         }
 
-        while (node != null) {
-            if (node.val != stack.pop()) {
+        // Compare the reversed second half with the first half
+        ListNode firstHalf = head;
+        ListNode secondHalf = prev;
+        while (secondHalf != null) {
+            // If values are different, then it's not a palindrome
+            if (secondHalf.val != firstHalf.val) {
                 return false;
             }
-            node = node.next;
+
+            // Move to the next nodes in both halves
+            secondHalf = secondHalf.next;
+            firstHalf = firstHalf.next;
         }
+
+        // All values matched, so it's a palindrome
         return true;
     }
 }
