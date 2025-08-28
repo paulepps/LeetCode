356927db04c1dc44353ea404b3ccb387f210a800
Paul Epps <paul@eppsnet.com>
2025-08-28 13:47:00 -0700

    Time: 2 ms (51.08%), Space: 41.2 MB (85.22%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,29 +1,18 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-        Queue<Integer> q = new LinkedList<>();
-
-        for (int i = 0; i < tickets.length; i++){
-           q.add(i);
+        int c=0 , i=0;
+        int n= tickets.length;
+        while(tickets[k]>0){
+            if(tickets[i]>0){
+                tickets[i]--;
+                c++;
+            }
+            i++;
+            //System.out.println(c);
+            if(i==n){
+                i=0;
+            }
         }
-
-        int minutes = 0;
-
-        while(!q.isEmpty()){
-            minutes++;
-
-            int front = q.poll();
-            
-            if(tickets[front] >= 1){
-                tickets[front] -= 1;
-            }
-            if(k == front && tickets[front] == 0){
-                break;
-            }
-            if(k != front && tickets[front] == 0){
-                continue;
-            }
-            q.add(front);
-        }
-        return minutes;
+        return c;
     }
 }
