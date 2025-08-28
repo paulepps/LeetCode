c94bf20dbc068721112f1915cc34953701886aaf
Paul Epps <paul@eppsnet.com>
2025-08-28 12:29:53 -0700

    Time: 4 ms (79.08%), Space: 44.6 MB (51.23%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,38 +1,26 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-
-    private ListNode mergeTwo(ListNode a, ListNode b) {
-        if (a == null) return b;
-        if (b == null) return a;
-
-        if (a.val <= b.val) {
-            a.next = mergeTwo(a.next, b);
-            return a;
-        } else {
-            b.next = mergeTwo(a, b.next);
-            return b;
-        }
-    }
     public ListNode mergeKLists(ListNode[] lists) {
-        
-        int k = lists.length;
+        PriorityQueue<ListNode> heap 
+            = new PriorityQueue<>((a, b) -> a.val - b.val);
 
-        if (k == 0) return null;
+        for(ListNode node : lists){
+            if(node != null)
+                heap.add(node);
+        }
 
-        for (int gap = 1; gap < k; gap *= 2) {
-            for (int i = 0; i + gap < k; i += gap * 2) {
-                lists[i] = mergeTwo(lists[i], lists[i + gap]);
+        ListNode head = new ListNode(0);
+        ListNode current = head;
+
+        while(!heap.isEmpty()){
+            ListNode node = heap.poll();
+            current.next = node;
+            current = current.next;
+
+            if (node.next != null) {
+                heap.add(node.next);
             }
         }
-        return lists[0];
+        
+        return head.next;
     }
 }
