791cdbf776c730897db2bf40867ff5f8ed89aba7
Paul Epps <paul@eppsnet.com>
2025-09-02 18:46:50 -0700

    Time: 6 ms (85.29%), Space: 44.4 MB (66.27%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -1,26 +1,15 @@
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
         
-        List<int[]> listOfArrays = new ArrayList<>();
-
-        for (int[] innerArray : intervals) {
-            listOfArrays.add(innerArray);
-        }
-
-        Collections.sort(listOfArrays, (arr1, arr2) -> {
-            int compareFirst = Integer.compare(arr1[0], arr2[0]);
-            if (compareFirst != 0) {
-                return compareFirst;
-            } else {
-                return Integer.compare(arr2[1], arr1[1]);
-            }
-        });
+        Arrays.sort(intervals, (arr1, arr2) -> (
+            arr1[0] == arr2[0] ? arr2[1] - arr1[1] : arr1[0] - arr2[0]
+        )); 
 
         int count = 1;
-        int firstIntervalEnd = listOfArrays.get(0)[1];
+        int firstIntervalEnd = intervals[0][1];
 
-        for (int i = 1; i < listOfArrays.size(); i++) {
-            int secondIntervalEnd = listOfArrays.get(i)[1];
+        for (int i = 1; i < intervals.length; i++) {
+            int secondIntervalEnd = intervals[i][1];
             if (secondIntervalEnd > firstIntervalEnd) {
                 count++;
                 firstIntervalEnd = secondIntervalEnd;
