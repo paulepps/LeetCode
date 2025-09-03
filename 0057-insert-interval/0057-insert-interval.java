7450cb922abc351d0fcfdc5b669c0b5e1be0052e
Paul Epps <paul@eppsnet.com>
2025-09-03 14:41:21 -0700

    Time: 1 ms (98.88%), Space: 44.8 MB (78.47%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,35 +1,26 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        
-        int[][] newIntervals = new int[intervals.length + 1][2];
+        List<int[]> result = new ArrayList<>();
+        int i = 0;
+        int n = intervals.length;
 
-        for (int i = 0; i < intervals.length; i++) {
-            newIntervals[i] = intervals[i];
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
+            i++;
         }
 
-        newIntervals[intervals.length] = newInterval;
-
-        return merge(newIntervals);
-    }
-
-    int[][] merge(int[][] intervals) {
-        
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        
-        List<int[]> res = new ArrayList<>();
-        res.add(intervals[0]);
-
-        for (int i = 1; i < intervals.length; i++) {
-            int[] prev = res.get(res.size() - 1);
-            int[] curr = intervals[i];
-
-            if (curr[0] <= prev[1]) {
-                prev[1] = Math.max(prev[1], curr[1]);
-            } else {
-                res.add(curr);
-            }
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
         }
-        return res.toArray(new int[0][]);
-    }
+        result.add(newInterval); 
 
+        while (i < n) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
 }
