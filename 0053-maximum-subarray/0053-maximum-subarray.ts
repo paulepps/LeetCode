8e219ff6fb1033abcfbbda35674dd81a5d850068
Paul Epps <paul@eppsnet.com>
2025-09-20 15:40:13 -0700

    Time: 2 ms (74.21%), Space: 63.9 MB (90.32%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.ts
@@ -0,0 +1,13 @@
+function maxSubArray(nums: number[]): number {
+    let localMax = nums[0];
+    let globalMax = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        localMax = Math.max(nums[i], localMax + nums[i]);
+        if (localMax > globalMax) {
+            globalMax = localMax;
+        }
+    }
+
+    return globalMax;
+}
