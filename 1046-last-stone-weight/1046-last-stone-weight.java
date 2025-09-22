3e4ee18753b173ee1593afecf898366b43cda3fa
Paul Epps <paul@eppsnet.com>
2025-09-21 19:50:16 -0700

    Time: 1 ms (98.82%), Space: 41.4 MB (32.86%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        
+        PriorityQueue<Integer> Q = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int stone : stones) {
+            Q.offer(stone);
+        }
+
+        while (Q.size() > 1) {
+            int y = Q.poll();
+            int x = Q.poll();
+            
+            int result = y - x;
+
+            if (result > 0) {
+                Q.offer(result);
+            }
+        }
+        return Q.size() == 0 ? 0 : Q.peek();
+    }
+}
