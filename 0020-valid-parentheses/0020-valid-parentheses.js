ab25ce579d8e6d7be5656bae25dfae702bd0d036
Paul Epps <paul@eppsnet.com>
2025-09-29 17:43:17 -0700

    Time: 3 ms (63.23%), Space: 54.9 MB (86.59%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,17 +3,27 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    let stack = [];
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
-            stack.push(s[i]);
-        } else if (s[i] == ')') {
-            if (stack.pop() != '(') return false;
-        } else if (s[i] == '}') {
-            if (stack.pop() != '{') return false;
-        } else if (s[i] == ']') {
-            if (stack.pop() != '[') return false;
+    const pairs = {
+        ')': '(',
+        '}': '{',
+        ']': '[',
+    };
+
+    const opening = new Set(Object.values(pairs));
+    const stack = [];
+
+    for (const c of s) {
+        if (opening.has(c)) {
+            stack.push(c);
+        } else if (c in pairs) {
+            if (stack.length === 0 || stack.pop() !== pairs[c]) {
+                return false;
+            }
+        } else {
+            // Optional: handle unexpected characters
+            return false;
         }
     }
-    return stack.length == 0;
-};
+
+    return stack.length === 0;
+}
