557319dd6f20df94251e49b0070807a12a934649
Paul Epps <paul@eppsnet.com>
2025-09-29 18:01:56 -0700

    Time: 4 ms (74.2%), Space: 60 MB (44.66%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,13 +1,8 @@
 function isPalindrome(s: string): boolean {
-    let str: string = s.toLowerCase();
-    str = str.replace(/[^a-z0-9]/g, "");
-
-    if (str.length == 0) return true;
-
-    const reversed: string[] = str.split("");
-    reversed.reverse();
-    const rstr: string = reversed.join("");
-console.log(str, rstr)
-    return str === rstr;
-    
-};
+    const strClean = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    if (!strClean) {
+        return true;
+    }
+    const reversed = strClean.split('').reverse().join('');
+    return strClean === reversed;
+}
