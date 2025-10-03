cf87da1da2364bfd5f4da48bb2710274ac9dee1a
Paul Epps <paul@eppsnet.com>
2025-10-03 16:48:08 -0700

    Time: 3 ms (95.16%), Space: 45.4 MB (19.29%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,21 +1,41 @@
 class Solution {
+
+    String s1;
+    String s2;
+    int[][] memo;
+
     public int minDistance(String word1, String word2) {
-        return editDistance(word1, word2, word1.length(), word2.length());
+
+        s1 = word1;
+        s2 = word2;
+
+        int m = word1.length();
+        int n = word2.length();
+        
+        memo = new int[m + 1][n + 1];
+        
+        for (int[] arr : memo) {
+            Arrays.fill(arr, -1);
+        }
+        return editDistance(m, n);
     }
 
-    private int editDistance(String s1, String s2, int m, int n) {
+    private int editDistance(int m, int n) {
 
         if (m == 0) return n;
 
         if (n == 0) return m;
 
-        if (s1.charAt(m - 1) == s2.charAt(n - 1))
-            return editDistance(s1, s2, m - 1, n - 1);
+        if (memo[m][n] != -1) return memo[m][n];
 
-        int insert = editDistance(s1, s2, m, n - 1);    
-        int replace = editDistance(s1, s2, m - 1, n - 1);
-        int remove = editDistance(s1, s2, m - 1, n);    
-
-        return 1 + Math.min(insert, Math.min(remove, replace));
+        if (s1.charAt(m - 1) == s2.charAt(n - 1)) {
+            memo[m][n] = editDistance(m - 1, n - 1);
+        } else {
+            int insert = editDistance(m, n - 1);    
+            int replace = editDistance(m - 1, n - 1);
+            int remove = editDistance(m - 1, n);    
+            memo[m][n] = 1 + Math.min(insert, Math.min(remove, replace));
+        }
+        return memo[m][n];
     }
 }
