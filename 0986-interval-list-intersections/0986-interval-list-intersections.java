dba56bf9f65dbbfe892b47e1806788087634eebc
Paul Epps <paul@eppsnet.com>
2025-10-03 18:06:49 -0700

    Time: 3 ms (98.24%), Space: 45.8 MB (51.72%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.java
+++ 0986-interval-list-intersections/0986-interval-list-intersections.java
@@ -10,8 +10,10 @@ class Solution {
             int[] first = firstList[i];
             int[] second = secondList[j];
             if (isOverlap(first, second)) {
-                result.add(new int[]{Math.max(first[0],second[0]), Math.min(first[1], second[1])});
-            }
+                result.add(new int[]{Math.max(first[0],second[0]), 
+                                     Math.min(first[1],second[1])
+                                     });
+            } 
             if (first[1] > second[1])
                 j++;
             else if (second[1] > first[1]) 
