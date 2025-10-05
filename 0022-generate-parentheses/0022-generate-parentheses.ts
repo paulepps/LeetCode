8a17cd800f40f834d575cb1f6fbdb0d68473c513
Paul Epps <paul@eppsnet.com>
2025-10-04 18:30:40 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.ts
@@ -0,0 +1,22 @@
+function generateParenthesis(n: number): string[] {
+    const result: string[] = [];
+
+    function backtrack(openCount: number, closeCount: number, currentString: string): void {
+        if (openCount === n && closeCount === n) {
+            result.push(currentString);
+            return;
+        }
+
+        if (openCount < this.n) {
+            backtrack(openCount + 1, closeCount, currentString + "(");
+        }
+
+        if (closeCount < openCount) {
+            backtrack(openCount, closeCount + 1, currentString + ")");
+        }
+    }
+
+    backtrack(0, 0, "");
+
+    return result;   
+};
