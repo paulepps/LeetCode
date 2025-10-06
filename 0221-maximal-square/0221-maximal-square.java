2b96aa7ffc8f87745696f20c1438aa33fcead7e6
Paul Epps <paul@eppsnet.com>
2025-10-06 16:36:51 -0700

    Time: 8 ms (70.39%), Space: 59.9 MB (81.1%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -7,8 +7,8 @@ class Solution {
         
         int max = 0;
         
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == '1') {
                     dp[i + 1][j + 1] = 
                         Math.min(Math.min(dp[i][j + 1], dp[i + 1][j]), 
