0d8a683f648573a8d5dfb1a6b9de8b59dc6110a9
Paul Epps <paul@eppsnet.com>
2025-10-08 15:17:10 -0700

    Time: 8 ms (61.95%), Space: 45.6 MB (10.66%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -2,25 +2,45 @@ class Solution {
 
     private int [] nums;
     private int target;
+    private int totalSum;
+    private int[][] dp;
+    private int n;
 
     public int findTargetSumWays(int[] nums, int target) {
         this.nums = nums;
         this.target = target;    
+
+        n = nums.length;
+
+        totalSum = 0;
+        for (int i = 0; i < n; i++) {
+            totalSum += nums[i];
+        }
+
+        dp = new int [n][2 * totalSum + 1];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
 
         return findTotalWays(0, 0);
     }
 
     private int findTotalWays(int i, int sum) {
         
-        if (sum == target && i == nums.length) {
+        if (sum == target && i == n) {
             return 1;
         }
 
-        if (i >= nums.length) {
+        if (i >= n) {
             return 0;
         }
 
-        return findTotalWays(i + 1, sum + nums[i])
+        if (dp[i][sum + totalSum] != -1) {
+            return dp[i][sum + totalSum];
+        }
+
+        return dp[i][sum + totalSum]
+             = findTotalWays(i + 1, sum + nums[i])
              + findTotalWays(i + 1, sum - nums[i]);
     }
 }
