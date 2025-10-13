3959e314995db742a9e5009cb983147e6952b93d
Paul Epps <paul@eppsnet.com>
2025-10-13 14:55:52 -0700

    Time: 0 ms (100%), Space: 29.4 MB (24.02%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int MySqrt(int x) {
+
+        if (x == 0) return 0;
+
+        long n = x;
+        long lo = 1, hi = x;
+        long res = 1;
+        
+        while (lo <= hi) {
+            long mid = lo + (hi - lo) / 2;
+            
+            if (mid * mid == n){
+                res = mid;
+                break;
+            }
+
+            if (mid * mid < n){
+                res = mid;
+                lo = mid + 1;
+            }
+            else {
+                hi = mid - 1;
+            }
+        }
+        
+        return (int)res;         
+    }
+}
