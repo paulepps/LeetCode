a073098c76e5ba7b5cf4203d70a128583e3d55a5
Paul Epps <paul@eppsnet.com>
2025-10-13 15:20:18 -0700

    Time: 6 ms (2.95%), Space: 42.3 MB (5.23%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,24 +1,17 @@
 class Solution {
     public boolean isHappy(int n) {
-        Set<Integer> s = new HashSet<>();
+        Set<Integer> seen = new HashSet<>();
 
-        while (true) {
-            n = sumSquareDigits(n);
-            if (n == 1)
-                return true;
-            if (s.contains(n))
-                return false;
-            s.add(n);
-        }
-    }
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
 
-    int sumSquareDigits(int n) {
-        int num = 0;
-        while (n != 0) {
-            int digit = n % 10;
-            num += digit * digit;
-            n /= 10;
+            // Pure arithmetic: extract digits using % and / inside a stream
+            int sum = IntStream.iterate(n, x -> x > 0, x -> x / 10)
+                               .map(x -> (x % 10) * (x % 10))
+                               .sum();
+
+            n = sum;
         }
-        return num;
-    }
+
+        return n == 1;    }
 }
