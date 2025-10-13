9f29db0367b884f0aaee51443c4e25264ab557a5
Paul Epps <paul@eppsnet.com>
2025-10-13 15:41:14 -0700

    Time: 0 ms (100%), Space: 55.9 MB (39.58%) - LeetHub

--- 0202-happy-number/0202-happy-number.js
+++ 0202-happy-number/0202-happy-number.js
@@ -3,12 +3,24 @@
  * @return {boolean}
  */
 var isHappy = function(n) {
-    let nums = [];
-    while (true)
-        {
-            n = (n + "").split("").map(Number).reduce((a,b) => a + b * b, 0);
-            if (n === 1) return true;
-            if (nums.includes(n)) return false;
-            nums.push(n);
+    const seen = new Set();
+
+    while (true) {
+        let num = 0;
+        while (n !== 0) {
+            const digit = n % 10;
+            num += digit * digit;
+            n = Math.floor(n / 10);
         }
-};
+        n = num;
+
+        if (n === 1) {
+            return true;
+        }
+        if (seen.has(n)) {
+            return false;
+        }
+        seen.add(n);
+    }
+}
+
