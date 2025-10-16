a519434a2db3fe37c9b72f7975104cfc32130042
Paul Epps <paul@eppsnet.com>
2025-10-16 16:05:18 -0700

    Time: 1 ms (36.74%), Space: 40.6 MB (31.07%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int maxProduct = 0;
+            for (int j = 1; j < i; j++) {
+                maxProduct = Math.max(maxProduct, Math.max(j * (i - j), j * dp[i - j]));
+            }
+            dp[i] = maxProduct;
+        }
+        return dp[n];    
+    }
+}
