edbb9e9770a6474ce132dec45d16d3b7e6f0578d
Paul Epps <paul@eppsnet.com>
2025-10-15 23:43:43 -0700

    Time: 23 ms (83.93%), Space: 43.2 MB (39.21%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        
+        int count = 1;
+        int sq = count * count;
+        
+        while (sq <= n) {
+            for (int i = sq; i <= n; i++) {
+                dp[i] = Math.min(dp[i - sq] + 1, dp[i]);
+            }
+            count++;
+            sq = count * count;
+        }
+        
+        return dp[n];
+    }
+}
