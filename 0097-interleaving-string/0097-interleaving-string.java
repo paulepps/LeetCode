7446778a754675abf5509b8c42263857b0d51a67
Paul Epps <paul@eppsnet.com>
2025-11-12 17:40:59 -0800

    Time: 3 ms (66.38%), Space: 43.4 MB (11.42%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -8,29 +8,30 @@ class Solution {
             return false;
         }
 
-        boolean[][] dp = new boolean[m + 1][n + 1];
+        boolean[] prev = new boolean[n + 1];
+        boolean[] curr = new boolean[n + 1];
 
-        // Base case: empty strings interleave to form an empty string
-        dp[0][0] = true;
-
-        // Fill the first row (interleaving s1 with an empty s2)
-        for (int i = 1; i <= m; i++) {
-            dp[i][0] = dp[i - 1][0] && (s1.charAt(i - 1) == s3.charAt(i - 1));
-        }
+        prev[0] = true;
 
         // Fill the first column (interleaving s2 with an empty s1)
         for (int j = 1; j <= n; j++) {
-            dp[0][j] = dp[0][j - 1] && (s2.charAt(j - 1) == s3.charAt(j - 1));
+            prev[j] = prev[j - 1] && (s2.charAt(j - 1) == s3.charAt(j - 1));
         }
 
         // Fill the rest of the DP table
         for (int i = 1; i <= m; i++) {
+
+            curr[0] = prev[0] && (s1.charAt(i - 1) == s3.charAt(i - 1));
+
             for (int j = 1; j <= n; j++) {
-                dp[i][j] = (dp[i - 1][j] && (s1.charAt(i - 1) == s3.charAt(i + j - 1))) ||
-                           (dp[i][j - 1] && (s2.charAt(j - 1) == s3.charAt(i + j - 1)));
+                int k = i + j;
+                curr[j] = (prev[j] && (s1.charAt(i - 1) == s3.charAt(k - 1))) ||
+                           (curr[j - 1] && (s2.charAt(j - 1) == s3.charAt(k - 1)));
             }
+
+            prev = curr.clone();
         }
 
-        return dp[m][n];
+        return prev[n];
     }
 }
