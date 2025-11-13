e973326d1708de4885b4224ead8208153db14990
Paul Epps <paul@eppsnet.com>
2025-11-12 16:08:03 -0800

    Time: 1 ms (90.52%), Space: 42 MB (39.24%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -4,21 +4,33 @@ class Solution {
         if (s1.length() + s2.length() != s3.length())
             return false;
 
-        return isInterleaveRec(s1, s2, s3, 0, 0);
+        int[][] memo = new int[s1.length() + 1][s2.length() + 1];
+        for(int[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+        return isInterleaveRec(s1, s2, s3, 0, 0, memo);
     }
 
-    private boolean isInterleaveRec(String s1, String s2, String s3, int i, int j) {
+    private boolean isInterleaveRec(String s1, String s2, String s3, int i, int j, int[][] memo) {
         
         int k = i + j;
       
         if (i == s1.length() && j == s2.length() && k == s3.length()) 
             return true;    
-    
-        boolean b1 = (i < s1.length()) && (s3.charAt(k) == s1.charAt(i)) && 
-                   isInterleaveRec(s1, s2, s3, i + 1, j);
-        boolean b2 = (j < s2.length()) && (s3.charAt(k) == s2.charAt(j)) && 
-                   isInterleaveRec(s1, s2, s3, i, j + 1);
+
+        if (memo[i][j] != -1) {
+            return memo[i][j] == 1;
+        }
+
+        boolean b1 = false;
+        if (i < s1.length() && s3.charAt(k) == s1.charAt(i))
+            b1 = isInterleaveRec(s1, s2, s3, i + 1, j, memo);
+
+        boolean b2 = false;
+        if (j < s2.length() && s3.charAt(k) == s2.charAt(j))
+            b2 = isInterleaveRec(s1, s2, s3, i, j + 1, memo);
         
+        memo[i][j] = b1 || b2 ? 1 : 0;
         return b1 || b2;
     }
 }
