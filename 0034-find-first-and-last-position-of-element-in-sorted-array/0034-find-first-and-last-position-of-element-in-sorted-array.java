47534fd9f64efabb67213f9c211a00d7817d709c
Paul Epps <paul@eppsnet.com>
2026-01-02 15:38:29 -0800

    Time: 0 ms (100%), Space: 48.1 MB (49.38%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,48 +1,40 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        
-        int start = first(nums, 0, nums.length - 1, target);
+        int[] res = { -1, -1 };
 
-        if (start == -1) {
-            return new int[]{-1, -1};
+        int lo = 0;
+        int hi = nums.length - 1;
+        
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if (nums[mid] < target) {
+                lo = mid + 1;
+            } else if (nums[mid] > target) {
+                hi = mid - 1;
+            } else {
+                res[0] = mid;
+                hi = mid - 1;
+            }
         }
-
-        int end = last(nums, 0, nums.length - 1, target);
-    
-        return new int[]{start, end};
-    }
-
-    private int first(int[] arr, int lo, int hi, int target) {
-        System.out.println(lo + " " + hi);
-        if (hi >= lo) {
-            int mid = lo + (hi - lo) / 2;
-
-            if ((mid == 0 || target > arr[mid - 1])
-                    && arr[mid] == target) {
-                return mid;
-            } else if (target > arr[mid]) {
-                return first(arr, mid + 1, hi, target);
-            } else {
-                return first(arr, lo, mid - 1, target);
-            }
-        } 
-        return -1;
-    }
-
-    private int last(int[] arr, int lo, int hi, int target) {
         
-        if (hi >= lo) {
-            int mid = lo + (hi - lo) / 2;
-
-            if ((mid == arr.length - 1 || target < arr[mid + 1])
-                    && arr[mid] == target) {
-                return mid;
-            } else if (target < arr[mid]) {
-                return last(arr, lo, mid - 1, target);
+        if (res[0] == -1)
+            return res;
+        
+        lo = res[0];
+        hi = nums.length - 1;
+        
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if (nums[mid] < target) {
+                lo = mid + 1;
+            } else if (nums[mid] > target) {
+                hi = mid - 1;
             } else {
-                return last(arr, mid + 1, hi, target);
+                res[1] = mid;
+                lo = mid + 1;
             }
-        } 
-        return -1;
+        }
+        
+        return res;
     }
 }
